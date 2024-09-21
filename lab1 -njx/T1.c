63d609b12a1f4d55ab71b5c76eb71dfbfa529406
Star-Sirius <147528552+Star-Sirius@users.noreply.github.com>
2024-09-21 18:44:18 +0800

    add函数

---
+++ lab1 -njx/T1.c
@@ -0,0 +1,15 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int sum = add(3, 5);
+    printf("The sum is: %d\n", sum);
+    return 0;
+}
